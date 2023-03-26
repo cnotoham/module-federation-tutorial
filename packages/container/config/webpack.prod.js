24c737289b043eb27c25053be211a3199d028e8e
Cornelius Notohamiprodjo <cnotoham@users.noreply.github.com>
2023-03-26 19:34:39 +1100

    Feat/aws cicd (#5)
    * Add gitignore and update webpack.prod.js adding publicPath
    
    * fix spelling error
    
    ---------
    
    Co-authored-by: Cornelius Notohamiprodjo <cnotoham@gmail.com>

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/laatest/',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({

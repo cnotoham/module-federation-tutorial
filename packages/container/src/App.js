44e6335c2a8eae63f6d0b41d2b0d5243ecca840f
Cornelius Notohamiprodjo <cnotoham@gmail.com>
2023-03-28 01:15:51 +1100

    fix css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <MarketingApp />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
